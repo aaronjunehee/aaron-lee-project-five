f5d28d7d90364ef07d5cf1d9d343adf501068f93
Aaron <aaronjunehee@gmail.com>
2019-10-12 19:04:58 -0400

    added scroll snap

--- src/Components/App.js
+++ src/Components/App.js
@@ -42,27 +42,27 @@ class App extends Component {
       <div>
         <Nav />
         <Header />
-          <main>
+        <main>
           <section className="formSection" name="form">
-              <Form />
-            </section>
+            <Form />
+          </section>
           <section className="commentSection" name="comments">
-              <div className="wrapper">
-                <ul className="userCommentsContainer">
-                  {this.state.userComments.map(userComments => {
-                    return ( 
-                      <UserComment 
-                        key={userComments.key}
-                        message={userComments.content.comment}
-                        numOfKills={userComments.content.count}
-                        gif={userComments.content.gif}
-                      />
-                    );
-                  })}
-                </ul>
-              </div>
-            </section>
-          </main>
+            <div className="wrapper">
+              <ul className="userCommentsContainer">
+                {this.state.userComments.map(userComments => {
+                  return ( 
+                    <UserComment 
+                      key={userComments.key}
+                      message={userComments.content.comment}
+                      numOfKills={userComments.content.count}
+                      gif={userComments.content.gif}
+                    />
+                  );
+                })}
+              </ul>
+            </div>
+          </section>
+        </main>
         {/* add a footer here */}
       </div>
     );
